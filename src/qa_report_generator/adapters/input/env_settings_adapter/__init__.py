7f3a8c5a07ce9eb599baa5463217ec68e2daafd8
Ondrej Winter <ondrej.winter@gmail.com>
2026-03-10 22:16:03 +0100

    refactor(env): narrow env settings adapter exports

--- src/qa_report_generator/adapters/input/env_settings_adapter/__init__.py
+++ src/qa_report_generator/adapters/input/env_settings_adapter/__init__.py
@@ -1,10 +1,7 @@
 """Environment configuration input adapter."""
 
 from .adapter import EnvSettingsAdapter
-from .settings import EnvSettings, load_settings_from_env
 
 __all__ = [
-    "EnvSettings",
     "EnvSettingsAdapter",
-    "load_settings_from_env",
 ]
